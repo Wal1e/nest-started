89269315ff062d30a1090aa7d8531f8056f05a1c
wangshangzhe <987261988@qq.com>
2023-03-13 11:27:37 +0800

    feat: 添加cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,15 @@ import { TransfromInterceptor } from './core/interceptor/transfrom.interceptor';
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('bzl/bosshi');
+  // 启用cors
+  app.enableCors();
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
+  // app.setGlobalPrefix('test');
   // 全局注册过滤器，对异常层具有完全控制权
   // 注意：AllExceptionsFilter 要在 HttpExceptionFilter 的上面，否则 HttpExceptionFilter 就不生效了，全被 AllExceptionsFilter 捕获了。
   app.useGlobalFilters(new AllExceptionFilter());
